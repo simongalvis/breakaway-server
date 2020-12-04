b8b84b51bf9133e563ff401f3fcc649250e2ab30
Simon Galvis <simongalvis9@gmail.com>
2020-12-03 21:42:07 -0700

    Create and populate seeds directory

---
+++ seeds/seed.breakaway_activities.sql
@@ -0,0 +1,5 @@
+INSERT INTO breakaway_activities(user_id, title, duration, distance, description)
+VALUES
+(1,'Morning Jog', 47, 8.34,'Went before work and was feeling great. Thinking of starting a run club for The Local.'),
+(2,'Evening cool down', 108, 26.45,'Practicing for the London marathon. Feeling good about this one.'),
+(3,'Afternoon Warm-up', 25, 3.37,'Always nice to get a run in before practice.');
---
+++ seeds/seed.breakaway_users.sql
@@ -0,0 +1,5 @@
+INSERT INTO breakaway_users(fullname, username, password)
+VALUES
+('Trent Broccoli', 'tbroccoli018', 'ilikebroccoli'),
+('Mo Farah', 'mfarah021', 'mynameismo'),
+('Didier Drogba ', 'ddrogba009', 'ivorycoastforlyf');
