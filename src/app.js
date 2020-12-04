25af2b173d0424b85e4c733106cbdcfc2fdf104c
Simon Galvis <simongalvis9@gmail.com>
2020-12-03 19:42:48 -0700

    Set up cors

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const {CLIENT_ORIGIN} = require('./config');
 
 const app = express()
 
@@ -13,7 +14,9 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+  origin: CLIENT_ORIGIN
+}));
 
 app.get('/', (req, res) =>{
     res.send('Hello, world!')
--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ const app = require('./app')
 const { PORT } = require('./config')
 
 
+app.get('/api/*', (req, res) => {
+  res.json({ok: true});
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
