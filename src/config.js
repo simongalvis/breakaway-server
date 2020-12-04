1549ec0ea4b8e5ababc94abe099ca6454d4c160f
Simon Galvis <simongalvis9@gmail.com>
2020-12-03 23:35:10 -0700

    Add initial testing for activities endpoint

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/breakaway'
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/breakaway',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/breakaway-test'
   }
---
+++ test/activities-endpoints.spec.js
@@ -0,0 +1,44 @@
+const knex = require('knex')
+const supertest = require('supertest')
+const app = require('../src/app')
+const { makeArticlesArray, makeMaliciousArticle } = require('./articles.fixtures')
+const { makeUsersArray } = require('./users.fixtures')
+
+
+
+describe('Activities Endpoints', function(){
+let db
+
+before('make knex instance', () => {
+
+    db = knex({
+        client: 'pg',
+        connection: process.env.TEST_DATABASE_URL,
+      })
+      app.set('db', db)
+
+})
+
+after('disconnect from db', () => db.destroy())
+
+before('clean the table', () => db.raw('TRUNCATE breakaway_users, breakaway_activities RESTART IDENTITY CASCADE'))
+
+describe(`GET /api/activities`, () =>{
+context(`Given no articles`, () =>{
+    return supertest(app)
+        .get('api/activities')
+        .expect(200, [])
+})
+
+
+
+})
+
+
+
+
+
+
+
+
+})
