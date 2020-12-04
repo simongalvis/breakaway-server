c276a7c3750c534b8f5630fa3b877142cd02408d
Simon Galvis <simongalvis9@gmail.com>
2020-12-03 19:52:02 -0700

    Add node version to package.json

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ app.get('/', (req, res) =>{
     res.send('Hello, world!')
 })
 
+
  app.use(function errorHandler(error, req, res, next) {
        let response
        if (NODE_ENV === 'production') {
