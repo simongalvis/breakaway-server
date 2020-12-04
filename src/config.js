cffcf1fdf70796ac58f46693548e10c9dd0c2106
Simon Galvis <simongalvis9@gmail.com>
2020-12-03 21:17:40 -0700

    Create and populate migrations directory

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
+    DATABASE_URL: process.env.DATABASE_URL
   }
